6ef4fe07e99970b0d808d272657f78c74c2838f8
feche <federico.romero@10pines.com>
2021-05-14 12:37:25 -0300

    primera parte hecha en clase

--- src/Library.hs
+++ src/Library.hs
@@ -1,3 +1,84 @@
 module Library where
 import PdePreludat
 
+data Hamburguesa = UnaHamburguesa{
+    valorInicial :: Number,
+    ingredientes :: [Ingrediente]
+} deriving(Show,Eq)
+
+-- Carne = 20
+-- Pan = 2
+-- Panceta = 10
+-- Cheddar = 10
+-- Pollo = 10
+-- Curry = 5
+
+data Ingrediente = Pan | Carne | Panceta | Cheddar | Pollo | Curry deriving(Show,Eq)
+
+type  Dinero = Number
+
+precio :: Ingrediente -> Dinero
+precio Carne = 20
+precio Pan = 2
+precio Panceta = 10
+precio Cheddar = 10
+precio Pollo = 10
+precio Curry = 10
+
+-- cuartoDeLibra = es una hamburguesa de pan, carne, cheddar, pan y el valor inicial es de 20.
+
+cuartoDeLibra = UnaHamburguesa 20 [Pan,Carne,Cheddar,Pan]
+
+-- el precio de una hamburguesa es el valor inicial mas la sumatoria de los precios de sus ingredientes
+precioHamburguesa:: Hamburguesa -> Dinero
+precioHamburguesa hamburguesa = valorInicial hamburguesa + sum (map precio (ingredientes hamburguesa))
+
+--AgregarIngrediente: recibe un ingrediente y una hambrugesa lo agrega a la hamburguesa.
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente ingrediente (UnaHamburguesa valorInicial ingredientes) = UnaHamburguesa valorInicial (ingrediente : ingredientes)
+
+--Agrandar: cada vez que se agranda una hamburguesa se agrega otro ingrediente base (Carne | Pollo), 
+--se elige el ingrediente base a agregar según lo que ya haya en la hamburguesa 
+--(si había carne se agrega carne, si había pollo se agrega pollo, si había ambos da igual cual se agregue).
+
+agrandar :: Hamburguesa -> Hamburguesa
+-- agrandar (UnaHamburguesa valorInicial ingredientes) 
+--     | elem Carne ingredientes = agregarIngrediente Carne  (UnaHamburguesa valorInicial ingredientes) 
+--     | elem Pollo ingredientes = agregarIngrediente Pollo  (UnaHamburguesa valorInicial ingredientes) 
+
+agrandar hamburguesa = agregarIngrediente (ingredienteBase hamburguesa) hamburguesa
+
+ingredienteBase :: Hamburguesa -> Ingrediente
+-- ingredienteBase (UnaHamburguesa _ ingredientes)
+--     | elem Carne ingredientes =  Carne
+--     | elem Pollo ingredientes = Pollo
+
+ingredientesBases = [Pollo, Carne]
+
+-- ingredienteBase hamburguesa = head (filter (\ ingrediente -> elem ingrediente ingredientesBases) (ingredientes hamburguesa))
+
+esBase ingrediente = elem ingrediente ingredientesBases
+ingredienteBase hamburguesa = head (filter esBase (ingredientes hamburguesa))
+
+--descuento: recibe un % de descuento, y devuelve la hamburguesa con ese descuento aplicado al valor inicial.
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento porcentaje (UnaHamburguesa valorInicial ingredientes) = UnaHamburguesa (valorInicial - (porcentaje * valorInicial/100)) ingredientes
+
+-- la pdepBurguer que es un cuarto de libra agrandado 2 veces con panceta, cheddar y 20% de descuento. 
+--Su precio final deberia ser 110
+
+pdepBurguer :: Hamburguesa
+
+pdepBurguer =
+  (descuento 20 . agrandar . agrandar . agregarIngrediente Cheddar .  agregarIngrediente Panceta) cuartoDeLibra
+
+-- pdepBurguer =
+--  descuento 20 . agrandar . agrandar . agregarIngrediente Cheddar .  agregarIngrediente Panceta $ cuartoDeLibra
+
+-- pdepBurguer = 
+--     descuento 20 (agrandar (agrandar (agregarIngrediente Cheddar (agregarIngrediente Panceta  cuartoDeLibra))))
+
+-- pdepBurguer = 
+--  foldr ($) cuartoDeLibra [descuento 20, agrandar, agrandar, agregarIngrediente Cheddar, agregarIngrediente Panceta]
--- src/Spec.hs
+++ src/Spec.hs
@@ -6,9 +6,23 @@ import Control.Exception (evaluate)
 
 correrTests :: IO ()
 correrTests = hspec $ do
-   describe "xxx" $ do
-     it "xxxx" $ do
-       1 `shouldBe` 2
+   describe "hamburguesas" $ do
+     it "el precio del cuarto de libra es 54" $ do
+       precioHamburguesa cuartoDeLibra  `shouldBe` 54
+     it "al agregar un ingrediente nuevo a una hamburguesa con algunos ingredientes el resulto es una nueva hamburguesa con ese ingrediente agregado" $ do
+       agregarIngrediente Panceta  cuartoDeLibra    `shouldBe` UnaHamburguesa 20 [Panceta ,Pan,Carne,Cheddar,Pan]
+     it "al agregar un ingrediente que ya esta en  una hamburguesa con algunos ingredientes el resulto es una nueva hamburguesa con ese ingrediente agregado otra vez " $ do
+       agregarIngrediente Cheddar  cuartoDeLibra    `shouldBe` UnaHamburguesa 20 [Cheddar ,Pan,Carne,Cheddar,Pan]
+     it "al agregar un ingrediente  una hamburguesa sin ingredientes el resulto es una nueva hamburguesa con ese ingrediente unicamente " $ do
+       agregarIngrediente Cheddar  (UnaHamburguesa 20 [])   `shouldBe` UnaHamburguesa 20 [Cheddar]
+     it "al agrandar una hamburguesa cuyo ingrediente base es Carne va a agregarle otra Carne   " $ do
+       agrandar (UnaHamburguesa 20 [Pan,Carne])   `shouldBe` UnaHamburguesa 20 [Carne,Pan,Carne]
+     it "al agrandar una hamburguesa cuyo ingrediente base es Pollo va a agregarle otro Pollo   " $ do
+       agrandar (UnaHamburguesa 20 [Pan,Pollo])   `shouldBe` UnaHamburguesa 20 [Pollo,Pan,Pollo]
+     it "al aplicar un porcentaje de descuento a una hamburguesa se devuelve esa hamburguesa con su valorInicial reducido en el % de decuento" $ do
+       descuento 10 (UnaHamburguesa 20 [Pan,Pollo])   `shouldBe` UnaHamburguesa 18 [Pan,Pollo]
+     it "la pdep burguer vale 110" $ do
+       precioHamburguesa  pdepBurguer   `shouldBe` 110
 
 escribime :: Expectation
 escribime = implementame
